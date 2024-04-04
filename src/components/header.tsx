c38cfbdece45b3938a6279db5d2b02a1bd07fc1f
joaovcostas <jvc22102003@gmail.com>
2024-04-03 22:10:59 -0300

    update: replace -a- tags with -nav-link- components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import nlwUniteIicon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
@@ -6,12 +7,8 @@ export function Header() {
       <img src={nlwUniteIicon} alt="" />
 
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">
-          Eventos
-        </a>
-        <a href="" className="font-medium text-sm">
-          Participantes
-        </a>
+        <NavLink href="/">Eventos</NavLink>
+        <NavLink href="/attendee">Participantes</NavLink>
       </nav>
     </div>
   )
