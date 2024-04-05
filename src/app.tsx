d0e7baaf350d219a3a68f5941e7d546efd49ee54
joaovcostas <jvc22102003@gmail.com>
2024-04-04 22:04:12 -0300

    update: fix -attendees- import

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
-import { AttendeeList } from './components/attendee-list'
+import { AttendeesList } from './components/attendees-list'
 import { Header } from './components/header'
 
 export function App() {
   return (
     <div className="max-w-[1216px] mx-auto p-5 flex flex-col gap-5">
       <Header />
-      <AttendeeList />
+      <AttendeesList />
     </div>
   )
 }
