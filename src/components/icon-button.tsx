df045c192a7479c5bfb6fe6f60dfc7ad2dd83e86
joaovcostas <jvc22102003@gmail.com>
2024-04-04 21:21:56 -0300

    feat: add -isTransparent- prop

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,12 +1,18 @@
 import { ComponentProps } from 'react'
 
-interface IconButtonProps extends ComponentProps<'button'> {}
+interface IconButtonProps extends ComponentProps<'button'> {
+  isTransparent?: boolean
+}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ isTransparent, ...props }: IconButtonProps) {
   return (
     <button
-      className="bg-white/10 border border-white/10 rounded-md p-1.5"
       {...props}
+      className={
+        isTransparent
+          ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
+          : 'bg-white/10 border border-white/10 rounded-md p-1.5'
+      }
     />
   )
 }
