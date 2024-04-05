892fb9d8c3fa2f7ff2e8f52f88fcd7f4df11c6cd
joaovcostas <jvc22102003@gmail.com>
2024-04-04 22:32:05 -0300

    feat: add -page- verification to change -page-

--- src/components/attendees-list.tsx
+++ src/components/attendees-list.tsx
@@ -28,8 +28,26 @@ export function AttendeesList() {
   const [search, setSearch] = useState('')
   const [page, setPage] = useState(1)
 
+  const totalPages = Math.ceil(attendees.length / 10)
+
   function onSearchInputChange(ev: ChangeEvent<HTMLInputElement>) {
     console.log(ev.target.value)
+  }
+
+  function goToFirstPage() {
+    setPage(1)
+  }
+
+  function goToPreviousPage() {
+    setPage(page - 1)
+  }
+
+  function goToNextPage() {
+    setPage(page + 1)
+  }
+
+  function goToLastPage() {
+    setPage(totalPages)
   }
 
   return (
@@ -101,20 +119,26 @@ export function AttendeesList() {
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
                 <span>
-                  Página {page} de {Math.ceil(attendees.length / 10)}
+                  Página {page} de {totalPages}
                 </span>
 
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
