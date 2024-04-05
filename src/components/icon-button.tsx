8396b7f19125fc009e320ae4188751af839dc27e
joaovcostas <jvc22102003@gmail.com>
2024-04-04 22:34:49 -0300

    feat: use -twMerge- to verify -disabled- prop

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {
   isTransparent?: boolean
@@ -8,11 +9,11 @@ export function IconButton({ isTransparent, ...props }: IconButtonProps) {
   return (
     <button
       {...props}
-      className={
-        isTransparent
-          ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
-          : 'bg-white/10 border border-white/10 rounded-md p-1.5'
-      }
+      className={twMerge(
+        'border border-white/10 rounded-md p-1.5',
+        isTransparent ? 'bg-black/20' : 'bg-white/10',
+        props.disabled ? 'opacity-50' : null,
+      )}
     />
   )
 }
